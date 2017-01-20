052a223980b60eaf9d2bd362f201860b4775c24f
J. Holmes <jholmes@n.io>
2017-01-20 15:57:13 -0700

    remove old helpers

--- helpers/index.js
+++
@@ -1,6 +0,0 @@
-var types = require('node-sass').types;
-
-module.exports = {
-  'pow($base, $exp)': function(base, exp) { return types.Number(Math.pow(base.getValue(), exp.getValue()), base.getUnit()); },
-  'sqrt($val)': function(val) { return types.Number(Math.sqrt(val.getValue()), val.getUnit()); }
-};
